8ba5205e3c861baf6a21aded9445dac80a741c9f
Ademir Vieira <vieiraademir707@gmail.com>
2025-01-27 23:15:08 -0300

    java 12 horas

--- ja.java
+++ ja.java
@@ -1,12 +1,16 @@
+
+import javax.swing.JOptionPane;
+
+
 public class ja {
     public static void main(String[] args){
-        int x = 23;
-        long y = 32114124L;
-        float w = 32.2f;
-        double d = 32.2;
-        char f = 's';
-        System.out.println("Numeros aleatorios: " + x);
+        String names = JOptionPane.showInputDialog("enter name boy");
+        JOptionPane.showMessageDialog(null, names);
 
+        int age = Integer.parseInt(JOptionPane.showInputDialog("enter age"));
+        JOptionPane.showMessageDialog(null, age);
+        double height = Double.parseDouble(JOptionPane.showInputDialog("put you height:"));
+        JOptionPane.showMessageDialog(null, height);
 
-    }    
+    }
 }
