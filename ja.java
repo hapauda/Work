c7f6e95a09822a5488cad73acaaea97daf73b3ce
Ademir Vieira <vieiraademir707@gmail.com>
2025-01-27 22:51:29 -0300

    java 12 horas

---
+++ ja.java
@@ -0,0 +1,12 @@
+public class ja {
+    public static void main(String[] args){
+        int x = 23;
+        long y = 32114124L;
+        float w = 32.2f;
+        double d = 32.2;
+        char f = 's';
+        System.out.println("Numeros aleatorios: " + x);
+
+
+    }    
+}
